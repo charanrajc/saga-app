79c75c3bf678204cfea8dce820e95fb6b033a8fd
charanrajc <charan.cjava@gmail.com>
2018-08-14 15:43:50 +0530

    first commit

---
+++ sagas/product.js
@@ -0,0 +1,20 @@
+import { takeLatest, put, all } from "redux-saga/effects";
+import { GET_PRODUCTS } from "../actionTypes/product";
+import {
+  getProductsSuccess,
+  getProductsFailure
+} from "../actionCreators/product";
+
+function* getProducts() {
+  try {
+    const response = yield fetch("http://192.168.56.1:4000/products");
+    const products = yield response.json();
+    yield put(getProductsSuccess(products));
+  } catch (err) {
+    yield put(getProductsFailure(err));
+  }
+}
+
+export function* productsWatcher() {
+  yield all([takeLatest(GET_PRODUCTS, getProducts)]);
+}
